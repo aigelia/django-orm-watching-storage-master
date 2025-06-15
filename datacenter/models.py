bce0330bca3a433c883ceb56975a5df603870218
Katerina <e.mayakovskaya@gmail.com>
2025-06-15 12:11:31 +0300

    Add global constant for seconds in minute

--- datacenter/models.py
+++ datacenter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
 
 class Passcard(models.Model):
     is_active = models.BooleanField(default=False)
@@ -41,7 +42,7 @@ def get_duration(visit):
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
     hours = total_seconds // SECONDS_IN_HOUR
-    minutes = (total_seconds % SECONDS_IN_HOUR) // 60
+    minutes = (total_seconds % SECONDS_IN_HOUR) // SECONDS_IN_MINUTE
     return f"{hours} ч. {minutes} мин."
 
 
